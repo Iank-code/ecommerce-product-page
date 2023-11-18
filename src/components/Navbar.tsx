b69f48e361a07a13d850919bb4b4dd106335e9b5
Ian Kamau <kian99564@gmail.com>
2023-11-18 23:44:39 +0300

    Styling nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,51 @@
+import { NavLink } from "react-router-dom";
 import cart from "./../assets/navbar/iconCart.svg";
 import userImage from "./../assets/navbar/image-avatar.png";
 
 export default function Navbar() {
+  interface links {
+    name: string;
+    path: string;
+  }
+  const navLink: links[] = [
+    {
+      name: "Collections",
+      path: "/collections",
+    },
+    {
+      name: "Men",
+      path: "/collections",
+    },
+    {
+      name: "Women",
+      path: "/collections",
+    },
+    {
+      name: "About",
+      path: "/collections",
+    },
+    {
+      name: "Contact",
+      path: "/collections",
+    },
+  ];
   return (
     <div className="flex justify-between">
-      <div>
+      <div className="flex gap-10">
         <span>sneakers</span>
-        <div></div>
+
+        <div className="flex gap-3">
+          {navLink.map((link, index) => {
+            return (
+              <a key={index} href={link.path}>
+                {link.name}
+              </a>
+            );
+          })}
+        </div>
       </div>
 
-      <div >
+      <div>
         <img src={cart} alt="cart.svg" />
         <img src={userImage} alt="userImage.png" />
       </div>
